ee8780e33c99328396d794beac98b06a2b5e1feb
UtsavVadodariya <162439323+UtsavVadodariya@users.noreply.github.com>
2024-09-06 13:19:55 +0530

    Day-4

--- Day-4 Events and Conditional Rendering/src/Hooks/Funtion.jsx
+++ Day-4 Events and Conditional Rendering/src/Hooks/Funtion.jsx
@@ -1,36 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-export default function Funtion() {
-    const [count,setCount] = useState(0);
-    const [name,setName] = useState("utsav")
-    const [isVisible,setIsVisible] = useState(false)
+export default function Function() {
+    const [count, setCount] = useState(0);
+    const [name, setName] = useState("");
+    const [subject, setSubject] = useState("");
+    const [isVisible, setIsVisible] = useState(false);
 
-    const submit =(event) =>{
-        event.preventDefault()
-        console.log("form Submitted");
-        let obj ={
-            name: name
-        }
-        console.log(obj)
-    }
-  return (
-    <div>
-        <h1>{count}</h1>
+    const submit = (event) => {
+        event.preventDefault();
+        console.log("Form Submitted");
+        const obj = {
+            name: name,
+            subject: subject
+        };
+        console.log(obj);
+    };
 
-        <button onClick={()=> setCount(count +1)}>Increse</button>
-        <button onClick={()=> setCount(count -1)}>Decrese</button>
+    return (
+        <div>
+            <h1>{count}</h1>
 
-        <form onSubmit={(event)=> submit(event)}>
-            <input type="text" placeholder='Enter your Name'/>
-            <input type="text" placeholder='Enter your Subject'/>
+            <button onClick={() => setCount(count + 1)}>Increase</button>
+            <button onClick={() => setCount(count - 1)}>Decrease</button>
 
-            <button type='submit'>Submit</button>
-        </form>
+            <form onSubmit={submit}>
+                <input type="text" placeholder='Enter your Name' value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="text" placeholder='Enter your Subject' value={subject} onChange={(e) => setSubject(e.target.value)} />
+                <button type='submit'>Submit</button>
+            </form>
 
-        {
-            isVisible ? <h1>bye</h1>:<h1>hiiii</h1>
-        }
-        <button onClick={()=> setIsVisible(!isVisible)}>click</button>
-    </div>
-  )
+            {
+                isVisible ? <h1>Bye</h1> : <h1>Hii</h1>
+            }
+            <button onClick={() => setIsVisible(!isVisible)}>Click</button>
+        </div>
+    );
 }
