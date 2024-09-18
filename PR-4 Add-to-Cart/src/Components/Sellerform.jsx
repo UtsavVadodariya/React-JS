05c16022da8931a8fa4e6f8424a7ae8e60377bce
UtsavVadodariya <162439323+UtsavVadodariya@users.noreply.github.com>
2024-09-18 15:02:29 +0530

    PR-4

--- PR-4 Add-to-Cart/src/Components/Sellerform.jsx
+++ PR-4 Add-to-Cart/src/Components/Sellerform.jsx
@@ -14,12 +14,8 @@ import Select from '@mui/material/Select';
 function App() {
     const [page, setPage] = useState('home');
     const [category, setCategory] = useState('');
-    const [age, setAge] = useState('');
 
-    const handleChange = (event) => {
-        setAge(event.target.value);
-    };
-    const goToNextPage = () => {
+    const NextPage = () => {
         setPage('next');
     };
 
@@ -27,8 +23,6 @@ function App() {
     return (
 
         <div className='main'>
-
-
             {page === 'home' ? (
                 <div>
                     <h1>Seller Details</h1>
@@ -49,157 +43,156 @@ function App() {
                         </FormControl>
 
                         <TextField id="outlined-basic" label="GSTIN" variant="outlined" required />
-                        <button onClick={goToNextPage} type='submit'>Go to Next Page</button>
+                        <button onClick={NextPage} type='submit'>Go to Next Page</button>
 
                     </form>
                 </div>
             ) : (
                 <div>
                     <h1>Product Details</h1>
-<form action="">
-                    <FormControl fullWidth>
-                        <InputLabel id="demo-simple-select-label">Category</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={category}
-                            label="Category"
-                            onChange={(e) => setCategory(e.target.value)}
-                        >
-                            <MenuItem value={"Electronics"}>Electronics</MenuItem>
-                            <MenuItem value={"Beauty"}>Beauty</MenuItem>
-                            <MenuItem value={"Fashion"}>Fashion</MenuItem>
-                            <MenuItem value={"Grocery"}>Grocery</MenuItem>
-                            <MenuItem value={"Accessories"}>Accessories</MenuItem>
-                        </Select>
-                    </FormControl>
-
-
-                    {
-                        category == "Electronics" && <TextField id="outlined-basic" label="Product Name" variant="outlined" required />
-
-                    }
-                    {
-                        category == "Electronics" && <TextField id="outlined-basic" label="Brand Name" variant="outlined" required />
-                    }
-                    {
-                        category == "Electronics" && <TextField id="outlined-basic" label="MFG Date" variant="outlined" required />
-                    }
-                    {
-                        category == "Electronics" && <TextField id="outlined-basic" label="Warranty" variant="outlined" required />
-                    }
-                    {
-                        category == "Electronics" && <TextField id="outlined-basic" label="Return Period" variant="outlined" required />
-                    }
-                    {
-                        category == "Electronics" && <FormControl fullWidth sx={{ m: 1 }}>
-                            <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
-                            <OutlinedInput
-                                id="outlined-adornment-amount"
-                                startAdornment={<InputAdornment position="start">₹</InputAdornment>}
-                                label="Amount"
-                            />
+                    <form action="">
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={category}
+                                label="Category"
+                                onChange={(e) => setCategory(e.target.value)}>
+                                <MenuItem value={"Electronics"}>Electronics</MenuItem>
+                                <MenuItem value={"Beauty"}>Beauty</MenuItem>
+                                <MenuItem value={"Fashion"}>Fashion</MenuItem>
+                                <MenuItem value={"Grocery"}>Grocery</MenuItem>
+                                <MenuItem value={"Accessories"}>Accessories</MenuItem>
+                            </Select>
                         </FormControl>
-                    }
-
-                    {
-                        category == "Beauty" && <TextField id="outlined-basic" label="Product Name" variant="outlined" required />
-                    }
-                    {
-                        category == "Beauty" && <TextField id="outlined-basic" label="Brand Name" variant="outlined" required />
-                    }
-                    {
-                        category == "Beauty" && <TextField id="outlined-basic" label="MFG Date" variant="outlined" required />
-                    }
-                    {
-                        category == "Beauty" && <TextField id="outlined-basic" label="Return Period" variant="outlined" required />
-                    }
-                    {
-                        category == "Beauty" && <FormControl fullWidth sx={{ m: 1 }}>
-                            <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
-                            <OutlinedInput
-                                id="outlined-adornment-amount"
-                                startAdornment={<InputAdornment position="start">₹</InputAdornment>}
-                                label="Amount"
-                            />
-                        </FormControl>
-                    }
 
 
-                    {
-                        category == "Fashion" && <TextField id="outlined-basic" label="Product Name" variant="outlined" required />
-                    }
-                    {
-                        category == "Fashion" && <TextField id="outlined-basic" label="Brand Name" variant="outlined" required />
-                    }
-                    {
-                        category == "Fashion" && <TextField id="outlined-basic" label="MFG Date" variant="outlined" required />
-                    }
-                    {
-                        category == "Fashion" && <TextField id="outlined-basic" label="Return Period" variant="outlined" required />
-                    }
-                    {
-                        category == "Fashion" && <FormControl fullWidth sx={{ m: 1 }}>
-                            <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
-                            <OutlinedInput
-                                id="outlined-adornment-amount"
-                                startAdornment={<InputAdornment position="start">₹</InputAdornment>}
-                                label="Amount"
-                            />
-                        </FormControl>
-                    }
+                        {
+                            category == "Electronics" && <TextField id="outlined-basic" label="Product Name" variant="outlined" required />
 
-                    {
-                        category == "Grocery" && <TextField id="outlined-basic" label="Product Name" variant="outlined" required />
-                    }
-                    {
-                        category == "Grocery" && <TextField id="outlined-basic" label="Brand Name" variant="outlined" required />
-                    }
-                    {
-                        category == "Grocery" && <TextField id="outlined-basic" label="MFG Date" variant="outlined" required />
-                    }
-                    {
-                        category == "Grocery" && <TextField id="outlined-basic" label="EXP Date" variant="outlined" required />
-                    }
-                    {
-                        category == "Grocery" && <FormControl fullWidth sx={{ m: 1 }}>
-                            <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
-                            <OutlinedInput
-                                id="outlined-adornment-amount"
-                                startAdornment={<InputAdornment position="start">₹</InputAdornment>}
-                                label="Amount"
-                            />
-                        </FormControl>
-                    }
+                        }
+                        {
+                            category == "Electronics" && <TextField id="outlined-basic" label="Brand Name" variant="outlined" required />
+                        }
+                        {
+                            category == "Electronics" && <TextField id="outlined-basic" label="MFG Date" variant="outlined" required />
+                        }
+                        {
+                            category == "Electronics" && <TextField id="outlined-basic" label="Warranty" variant="outlined" required />
+                        }
+                        {
+                            category == "Electronics" && <TextField id="outlined-basic" label="Return Period" variant="outlined" required />
+                        }
+                        {
+                            category == "Electronics" && <FormControl fullWidth sx={{ m: 1 }}>
+                                <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+                                <OutlinedInput
+                                    id="outlined-adornment-amount"
+                                    startAdornment={<InputAdornment position="start">₹</InputAdornment>}
+                                    label="Amount"
+                                />
+                            </FormControl>
+                        }
+
+                        {
+                            category == "Beauty" && <TextField id="outlined-basic" label="Product Name" variant="outlined" required />
+                        }
+                        {
+                            category == "Beauty" && <TextField id="outlined-basic" label="Brand Name" variant="outlined" required />
+                        }
+                        {
+                            category == "Beauty" && <TextField id="outlined-basic" label="MFG Date" variant="outlined" required />
+                        }
+                        {
+                            category == "Beauty" && <TextField id="outlined-basic" label="Return Period" variant="outlined" required />
+                        }
+                        {
+                            category == "Beauty" && <FormControl fullWidth sx={{ m: 1 }}>
+                                <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+                                <OutlinedInput
+                                    id="outlined-adornment-amount"
+                                    startAdornment={<InputAdornment position="start">₹</InputAdornment>}
+                                    label="Amount"
+                                />
+                            </FormControl>
+                        }
 
 
-                    {
-                        category == "Accessories" && <TextField id="outlined-basic" label="Product Name" variant="outlined" required />
-                    }
-                    {
-                        category == "Accessories" && <TextField id="outlined-basic" label="Brand Name" variant="outlined" required />
-                    }
-                    {
-                        category == "Accessories" && <TextField id="outlined-basic" label="MFG Date" variant="outlined" required />
-                    }
-                    {
-                        category == "Accessories" && <TextField id="outlined-basic" label="Warranty" variant="outlined" required />
-                    }
-                    {
-                        category == "Accessories" && <TextField id="outlined-basic" label="Return Period" variant="outlined" required />
-                    }
-                    {
-                        category == "Accessories" && <FormControl fullWidth sx={{ m: 1 }}>
-                            <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
-                            <OutlinedInput
-                                id="outlined-adornment-amount"
-                                startAdornment={<InputAdornment position="start">₹</InputAdornment>}
-                                label="Amount"
-                            />
-                        </FormControl>
-                    }
+                        {
+                            category == "Fashion" && <TextField id="outlined-basic" label="Product Name" variant="outlined" required />
+                        }
+                        {
+                            category == "Fashion" && <TextField id="outlined-basic" label="Brand Name" variant="outlined" required />
+                        }
+                        {
+                            category == "Fashion" && <TextField id="outlined-basic" label="MFG Date" variant="outlined" required />
+                        }
+                        {
+                            category == "Fashion" && <TextField id="outlined-basic" label="Return Period" variant="outlined" required />
+                        }
+                        {
+                            category == "Fashion" && <FormControl fullWidth sx={{ m: 1 }}>
+                                <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+                                <OutlinedInput
+                                    id="outlined-adornment-amount"
+                                    startAdornment={<InputAdornment position="start">₹</InputAdornment>}
+                                    label="Amount"
+                                />
+                            </FormControl>
+                        }
 
-                    <button>Add Product</button>
+                        {
+                            category == "Grocery" && <TextField id="outlined-basic" label="Product Name" variant="outlined" required />
+                        }
+                        {
+                            category == "Grocery" && <TextField id="outlined-basic" label="Brand Name" variant="outlined" required />
+                        }
+                        {
+                            category == "Grocery" && <TextField id="outlined-basic" label="MFG Date" variant="outlined" required />
+                        }
+                        {
+                            category == "Grocery" && <TextField id="outlined-basic" label="EXP Date" variant="outlined" required />
+                        }
+                        {
+                            category == "Grocery" && <FormControl fullWidth sx={{ m: 1 }}>
+                                <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+                                <OutlinedInput
+                                    id="outlined-adornment-amount"
+                                    startAdornment={<InputAdornment position="start">₹</InputAdornment>}
+                                    label="Amount"
+                                />
+                            </FormControl>
+                        }
+
+
+                        {
+                            category == "Accessories" && <TextField id="outlined-basic" label="Product Name" variant="outlined" required />
+                        }
+                        {
+                            category == "Accessories" && <TextField id="outlined-basic" label="Brand Name" variant="outlined" required />
+                        }
+                        {
+                            category == "Accessories" && <TextField id="outlined-basic" label="MFG Date" variant="outlined" required />
+                        }
+                        {
+                            category == "Accessories" && <TextField id="outlined-basic" label="Warranty" variant="outlined" required />
+                        }
+                        {
+                            category == "Accessories" && <TextField id="outlined-basic" label="Return Period" variant="outlined" required />
+                        }
+                        {
+                            category == "Accessories" && <FormControl fullWidth sx={{ m: 1 }}>
+                                <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+                                <OutlinedInput
+                                    id="outlined-adornment-amount"
+                                    startAdornment={<InputAdornment position="start">₹</InputAdornment>}
+                                    label="Amount"
+                                />
+                            </FormControl>
+                        }
+
+                        <button>Add Product</button>
                     </form>
                 </div>
             )}
